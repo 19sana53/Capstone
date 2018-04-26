2c67bdcabea72e04b62321e018fce031c2cf34f8
19sana53 <19sana53@students.issh.ac.jp>
2018-04-26 14:02:06 +0900

    Add files via upload

--- Capstone_Sana.py
+++ Capstone_Sana.py
@@ -26,26 +26,33 @@ class Player(spgl.Sprite):
 		self.frame = 0 
 		
 	def tick(self): 	
-		if self.state == "jumping":	
-			self.sety(self.ycor() + self.y_speed)
-			self.y_acceleration += game.gravity 
-			self.y_speed += self.y_acceleration 
+		self.sety(self.ycor() + self.y_speed)
+		self.y_acceleration += game.gravity 
+		self.y_speed += self.y_acceleration 
 					
 		self.setx(self.xcor() + self.x_speed)
 		
 		for block in blocks:
-			if game.is_collision(self, block):
+			if game.is_collision(self, block) and self.ycor() > block.ycor():
+				print("BLOCK COLLISION")
 				self.y_acceleration = 0 
 				self.y_speed = 0 
-				self.sety(block.ycor() + block.height +1)
+				self.sety(block.ycor() + block.height)
 				self.state = "running"	
 		
 		if game.is_collision(self, pipe):
+			print("PIPE COLLISION")
 			self.y_acceleration = 0 
 			self.y_speed = 0 
-			self.sety(pipe.ycor() + pipe.height + 5)
+			self.sety(pipe.ycor() + pipe.height)
 			self.state = "running"
 
+		# Off screen
+		if self.ycor() < -300:
+			self.goto(300, -250)
+			self.y_acceleration = 0
+			self.y_speed = 0
+			self.x_speed = 0
 
 	def jump(self):
 		if self.state == "running": 
@@ -55,10 +62,10 @@ class Player(spgl.Sprite):
 			game.play_sound("jump.wav")
 			
 	def turn_left(self):
-		self.x_speed = -3 
+		self.x_speed = -5 
 
 	def turn_right(self):
-		self.x_speed = 3 
+		self.x_speed = 5
 
 class Rock(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
@@ -89,14 +96,14 @@ class Peach(spgl.Sprite):
 # Create Functions
 
 # Initial Game setup
-game = spgl.Game(800, 600, "black", "Sana Kureshi - SKYDIVER MARIO", 0)
+game = spgl.Game(800, 600, "black", "Sana Kureshi - SUPER MARIO BROS.", 5)
 game.coins = 10
 game.gravity = -0.3
 game.play_sound("theme_song.wav -v 0.6", 10) 
 
 # Create Sprites / player
 player = Player("triangle", "white", 300, -250)
-player.set_image("mario.gif", 25, 30)
+player.set_image("mario.gif", 50, 60)
 
 donkeykong = Donkeykong("circle", "red", 255, 255)
 donkeykong.set_image("donkeykong.gif", 50, 60) 
@@ -108,23 +115,26 @@ pipe = Pipe("triangle", "blue", 300, -250)
 pipe.set_image("pipe.gif", 80, 80)
 
 #ROCKS
-rock = Rock("circle", "red", 110, -55)
-rock.set_image("rock.gif", 25, 25)
+rocks = []
+rocks.append(Rock("rock.gif", "red", 140, -110))
+rocks.append(Rock("rock.gif", "red", 250, 90))
+rocks.append(Rock("rock.gif", "red", -210, 240))
 
-rock = Rock("circle", "red", 155, -100)
-rock.set_image("rock.gif", 25, 25)
+for rock in rocks: 
+	rock.set_bounding_box(25, 25)
 	
 #BLOCKS
 blocks = []
-blocks.append(Block("brick2.gif", "blue", 150 , -100))
+blocks.append(Block("brick2.gif", "blue", 150 , -150))
 blocks.append(Block("brick3.gif", "red", 210, 50))
 blocks.append(Block("brick2.gif", "red", -200, -150))
 blocks.append(Block("brick3.gif", "red",  -180, 200))
 blocks.append(Block("brick2.gif", "red",  -230, 30)) 
 blocks.append(Block("brick2.gif", "red",  260, 200))
+blocks.append(Block("brick2.gif", "red",  0, 0))
  
 for block in blocks:
-	block.set_bounding_box(75, 25)
+	block.set_bounding_box(150, 50)
 
 #COINS
 coins = []
@@ -135,8 +145,7 @@ coins.append(Coin("coins.gif", "blue", -180 , 250))
 
 for coin in coins: 
 	coin.set_bounding_box (30, 20)
-	
- 
+
 # Create Labels
 score_label = spgl.Label("Score : {}".format(game.coins), "white", -380, 280)
 game.set_background("background.gif")
@@ -152,26 +161,21 @@ while True:
      
 	if game.is_collision(player, rock): 
 		player.score -= 5
-		score_label.update("Score: {}".format(player.score))
+		score_label.update("SCORE: {}".format(player.score))
 		player.goto(300, -250)
 	else:
 		player.score += 0
 		
 	if game.is_collision(player, coin):  
 		player.score += 10
-		score_label.update("Score: {}".format(player.score))
-		player.goto()
+		score_label.update("SCORE: {}".format(player.score))
 	else: 
 		player.score += 0 
 		
-	if game.is_collision(player, donkeykong): 
-		player.score += 10
-		score_label.update("Score: {}".format(player.score))
+	if game.is_collision(player, peach): 
+		player.score += 20
+		score_label.update("SCORE: {}".format(player.score))
 	else: 
 		player.score += 0
 		
-	print(player.state, player.y_acceleration, player.y_speed)
-    	
-    
-    
-    
+	print(player.state, player.xcor(), player.ycor(), player.y_acceleration, player.y_speed)
